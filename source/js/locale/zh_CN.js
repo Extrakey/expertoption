c9c278ce21c974969f75933dac21fb0ee91415ff
miaomiao8890 <miaomiao8890@vip.qq.com>
2017-06-02 20:10:53 +0800

    add

--- source/js/locale/zh_CN.js
+++ source/js/locale/zh_CN.js
@@ -1,4 +1,5 @@
 const zh_CN = {
   'test': "测试",
+  'changeLanguage': "切换语言",
 }
 export default zh_CN;
--- source/js/locale/zh_TW.js
+++ source/js/locale/zh_TW.js
@@ -1,4 +1,5 @@
 const zh_TW = {
   'test': "测试",
+  'changeLanguage': "切换语言",
 }
 export default zh_TW;
