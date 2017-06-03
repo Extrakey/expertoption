9e2ae5c3c4ea3ed2d1a377f6c4f690a5e62bc623
jiangnan <229142144@qq.com>
2017-06-03 19:25:44 +0800

    merge

--- source/js/views/User/Edit.jsx
+++ source/js/views/User/Edit.jsx
@@ -15,8 +15,47 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className='App flex column flex1'>
-        编辑个人资料
+      <div className='App flex flex1'>
+        <div className="flex1">
+          <h5>625-781-905</h5>
+          <p>账号 625-781-905</p>
+          <h5>地址</h5>
+          <p>国家</p>
+          <input type="text" placeholder="hk"/>
+          <p>城市</p>
+          <input type="text" placeholder="hk"/>
+          <p>街道地址</p>
+          <input type="text" placeholder="hk"/>
+        </div>
+        <div className="flex1">
+          <h5>个人资料</h5>
+          <p>性别</p>
+          <select name="" id="">
+            <option value="0">男</option>
+            <option value="1">女</option>
+          </select>
+          <p>生日</p>
+          <select name="" id="">
+            <option value="0">男</option>
+          </select>
+          <select name="" id="">
+            <option value="0">男</option>
+          </select>
+          <select name="" id="">
+            <option value="0">男</option>
+          </select>
+          <p>联系电话</p>
+          <input type="text"/>
+          <input type="text"/>
+          <p>电子邮箱</p>
+          <input type="text" value="229142144@qq.com"/>
+          <button className="button">保存更改</button>
+        </div>
+        <div className="flex1">
+          <h5>头像</h5>
+          <input type="file"/>
+          <button className="button">上传</button>
+        </div>
       </div>
     );
   }
