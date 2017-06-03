73e276214b5c2fc7e8edfa10fc50d6885d3adfcc
jiangnan <229142144@qq.com>
2017-06-03 14:09:28 +0800

    merge

--- source/js/components/Global/Confirm.jsx
+++ source/js/components/Global/Confirm.jsx
@@ -1,27 +1,20 @@
 /**
  * Created by jiangnan on 17/6/2.
  */
-/**
- * Created by jiangnan on 17/6/2.
- */
-/**
- * Created by jiangnan on 17/5/31.
- */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { IndexLink, Link } from 'react-router';
 import { routeCodes } from '../../routes';
 import TabNav from './TabNav'
+import * as appAction from 'actions/app';
 
 export default class Modal extends Component {
-
-
   render() {
     const { title } = this.props
     return (
-      <div className="wrapper_black" onClick={hideSetting}>
+      <div className="wrapper_black" onClick={hide}>
         <div>
-          <h4>{title}</h4>
+          <h4>{title} <span>关闭</span></h4>
           <div>
             <button>确定</button>
             <button>取消</button>
--- source/js/views/Transaction/index.jsx
+++ source/js/views/Transaction/index.jsx
@@ -3,29 +3,52 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import * as transactionAction from 'actions/transaction';
+import * as appAction from 'actions/transaction';
 import { routeCodes } from '../../routes';
 import Radium from 'radium'
 import Confirm from 'components/Global/Confirm'
+import NoData from 'components/Global/NoData'
+
 
 @connect(state => ({
   list: state.trans.get('list').toJS(),
-}), dispatch => bindActionCreators(transactionAction, dispatch))
+}), dispatch => bindActionCreators(transactionAction,  dispatch))
 export default class Transaction extends Component {
   static propTypes = {
     children: PropTypes.object,
+  }
+
+  constructor() {
+    super()
+    this.state={
+      noData: true
+    }
   }
 
   componentDidMount() {
     this.props.getUserTrans()
   }
 
+  hideNoData() {
+    this.setState({
+      noData: false
+    })
+  }
+
 
   render() {
     const { list } = this.props
+    const { noData } = this.state
     return (
       <div className='container'>
         交易
-        {/*{!list.length && <Confirm />}*/}
+        {!list.length && noData &&
+        <div className="wrapper_black" onClick={::this.hideNoData}>
+          <div className="wrapper_white">
+            <NoData linkTo={routeCodes.BILLING} buttonText="入金" title="请存入资金，用真实的钱款进行交易"/>
+          </div>
+        </div>
+        }
       </div>
     );
   }
